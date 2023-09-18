9917712e6a4f67b9f5b39f895465ab7a7ff17c73
motsukov <motsukov@gmail.com>
2023-09-18 18:10:12 +0300

    Freature email verification modal was added

--- src/2_pages/home/ui/home.page.tsx
+++ src/2_pages/home/ui/home.page.tsx
@@ -1,15 +1,22 @@
 import { FC } from "react";
 import { LogoutFeature } from "../../../4_features/auth-features";
 import { authFirebase } from "../../../6_shared";
+
+import { useIdToken } from "react-firebase-hooks/auth";
+
 export const HomePage: FC = () => {
 	if (!authFirebase.currentUser) throw new Error("No user");
+	const [user] = useIdToken(authFirebase);
 
+	if (!user) throw new Error("No user");
 
 	return (
 		<div>
 			<h1>Home page</h1>
 			<p>Welcome, {authFirebase.currentUser.displayName}</p>
+			{!user.emailVerified && <p>Email {user.email} not verified</p>}
 			<LogoutFeature />
+
 		</div>
 	);
 };
---
+++ src/4_features/auth-features/email-verification-modal/ui/email-verification-miodal.component.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react";
+import { Modal, authFirebase, useModal } from "../../../../6_shared";
+
+import { useSendEmailVerification } from "react-firebase-hooks/auth";
+import Button from "../../../../6_shared/ui/button/button.component";
+import { getAuth } from "firebase/auth";
+
+export const EmailVerificationModal: FC = () => {
+	const [sand] = useSendEmailVerification(authFirebase);
+	const user = getAuth().currentUser;
+	const [isVisible, toggleModal] = useModal(true);
+
+	if (!user) throw new Error("No user");
+
+	const sandVerificationHandle = () => {
+		sand().then(() => {
+			toggleModal();
+		});
+	};
+
+	return (
+		isVisible && (
+			<Modal title="Email not verified" toggleModal={toggleModal}>
+				<h1>Please verify your email: {user.email}</h1>
+				<Button onClick={sandVerificationHandle}>
+					Send verification email
+				</Button>
+			</Modal>
+		)
+	);
+};
