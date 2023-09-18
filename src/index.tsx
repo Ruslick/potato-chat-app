4478c1a9f3a8c16b70ded051bf5e3ca215d3689a
AleksanderIazev <mr.robot88@inbox.ru>
2023-09-18 21:43:34 +0300

    added Eslint Prettier Husky for code style

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./1_app/app";
 import "firebase/firestore";
-import GlobalStyles from "./6_shared/lib/global.styles";
+import GlobalStyles from "./1_app/app/global.styles";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
