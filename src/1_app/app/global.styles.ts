7d97dea1491adcfa12688dd05e315fc0e7c4d175
AleksanderIazev <mr.robot88@inbox.ru>
2023-09-25 22:01:37 +0300

    add loginWrapper

--- src/1_app/app/global.styles.ts
+++ src/1_app/app/global.styles.ts
@@ -1,9 +1,20 @@
 import { createGlobalStyle } from 'styled-components';
+import { globalTheme } from '../../6_shared/lib/global.theme';
 
 export const GlobalStyle = createGlobalStyle`
+  
+
+
   body {
     margin: 0;
     padding:0;
     box-sizing: border-box;
+    font-weight: ${globalTheme.fontWeights.normal};
+    /* background-color: ${globalTheme.colors.lightOrange}; */
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 `;
